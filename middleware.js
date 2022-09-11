a1ef89ce593a3051491f7f78adf061fd44b9ed5f
MVAodhan <hamilton.aodhan@gmail.com>
2022-09-11 22:45:28 +1200

    why is it re rendering?

--- middleware.js
+++ middleware.js
@@ -3,7 +3,6 @@ import { NextResponse } from 'next/server';
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
   const { pathname } = request.nextUrl;
-  console.log(pathname);
 }
 
 // See "Matching Paths" below to learn more
