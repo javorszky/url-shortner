6d4753911d99805d99b7b19b3331e8d4305fe8bb
MVAodhan <hamilton.aodhan@gmail.com>
2022-09-11 21:25:56 +1200

    remove dependency array

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
     (async () => {
       await refreshLinks();
     })();
-  }, []);
+  });
 
   const refreshLinks = async () => {
     const linksObject = await getLinks();
