e1537ba884be90d32893d52841625f935cd959ef
MVAodhan <hamilton.aodhan@gmail.com>
2022-09-19 22:35:45 +1200

    sending a status

--- pages/api/go/[code].js
+++ pages/api/go/[code].js
@@ -1,9 +1,10 @@
 import { getLink } from '../../../utils/getUtils';
+import { NextResponse } from 'next/server';
 
 export default async function handler(req, res) {
   const { code } = req.query;
 
   const longUrl = await getLink(code);
 
-  res.redirect(longUrl);
+  res.redirect(307, longUrl);
 }
